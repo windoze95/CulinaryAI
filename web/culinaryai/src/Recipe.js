5d1a9ded82b7a9f13c83d4146deeaec07842f900
windoze95 <19397727+windoze95@users.noreply.github.com>
2023-09-22 19:13:42 -0600

    display fetched recipe

--- web/culinaryai/src/Recipe.js
+++ web/culinaryai/src/Recipe.js
@@ -2,6 +2,42 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useAuth } from './App';
 
+const IngredientList = ({ ingredients }) => (
+    <ul>
+      {ingredients.map((ingredient, index) => (
+        <li key={index}>
+          {ingredient.amount} {ingredient.unit} of {ingredient.name}
+        </li>
+      ))}
+    </ul>
+);
+
+const InstructionsList = ({ instructions }) => (
+    <ol>
+        {instructions.map((instruction, index) => (
+        <li key={index}>{instruction}</li>
+        ))}
+    </ol>
+);
+
+const RecipeDetail = ({ mainRecipe, subRecipes }) => (
+    <div>
+        <h2>Main Recipe</h2>
+        <IngredientList ingredients={mainRecipe.ingredients} />
+        <InstructionsList instructions={mainRecipe.instructions} />
+        <p>Time to cook: {mainRecipe.timeToCook} minutes</p>
+        
+        {subRecipes.map((subRecipe, index) => (
+        <div key={index}>
+            <h3>Sub Recipe {index + 1}</h3>
+            <IngredientList ingredients={subRecipe.ingredients} />
+            <InstructionsList instructions={subRecipe.instructions} />
+            <p>Time to cook: {subRecipe.timeToCook} minutes</p>
+        </div>
+        ))}
+    </div>
+);
+
 const Recipe = ({ match }) => {
     const { isAuthenticated, user } = useAuth();
     const [recipe, setRecipe] = useState(null);
@@ -41,9 +77,11 @@ const Recipe = ({ match }) => {
             <p>Generating your recipe...</p>
         ) : (
             <div>
+                <h1>{recipe.Title}</h1>
                 {/* Display your recipe here */}
+                {recipe && <RecipeDetail mainRecipe={recipe.FullRecipe.MainRecipe} subRecipes={recipe.FullRecipe.SubRecipes} />}
                 {isAuthenticated && recipe.GeneratedByUserID === user.ID && (
-                <button onClick={regenerateRecipe}>Regenerate</button>
+                    <button onClick={regenerateRecipe}>Regenerate</button>
                 )}
             </div>
         )}
