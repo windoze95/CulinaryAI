eb2d138cead6ac23476ddef67fa6464e7e663fa5
windoze95 <19397727+windoze95@users.noreply.github.com>
2023-09-29 00:26:44 -0600

    fix login failure after generate

--- internal/service/user.go
+++ internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -71,6 +72,9 @@ func (s *UserService) LoginUser(username, password string) (*models.User, error)
 	if err != nil {
 		return nil, err
 	}
+
+	log.Printf("User: %+v", user)
+	log.Printf("HashedPassword: %s", user.HashedPassword)
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password)); err != nil {
 		return nil, errors.New("invalid username or password")
