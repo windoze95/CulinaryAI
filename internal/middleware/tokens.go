5e783652039cb18159d724e5df8e7033e7be0863
windoze95 <19397727+windoze95@users.noreply.github.com>
2023-10-21 21:29:14 -0600

    use authorization token for protected routes

--- internal/middleware/tokens.go
+++ internal/middleware/tokens.go
@@ -6,54 +6,26 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/windoze95/saltybytes-api/internal/config"
-	"github.com/windoze95/saltybytes-api/internal/util"
 )
-
-func VerifyTokenMiddleware(cfg *config.Config) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		cookie, err := c.Cookie("auth_token") // Fetch auth_token cookie
-		if err != nil {
-			util.ClearAuthTokenCookie(c)
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "No token provided", "forceLogout": true})
-			c.Abort()
-			return
-		}
-
-		tokenString := cookie // Token is fetched from the cookie
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(cfg.Env.JwtSecretKey.Value()), nil
-		})
-		if err != nil {
-			util.ClearAuthTokenCookie(c)
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token", "forceLogout": true})
-			c.Abort()
-			return
-		}
-
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			c.Set("user_id", claims["user_id"])
-			c.Next()
-		} else {
-			util.ClearAuthTokenCookie(c)
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "forceLogout": true})
-			c.Abort()
-			return
-		}
-	}
-}
 
 // func VerifyTokenMiddleware(cfg *config.Config) gin.HandlerFunc {
 // 	return func(c *gin.Context) {
-// 		authHeader := c.GetHeader("Authorization")
-// 		tokenString := authHeader // Token is directly provided in the Authorization header
+// 		cookie, err := c.Cookie("auth_token") // Fetch auth_token cookie
+// 		if err != nil {
+// 			util.ClearAuthTokenCookie(c)
+// 			c.JSON(http.StatusUnauthorized, gin.H{"message": "No token provided", "forceLogout": true})
+// 			c.Abort()
+// 			return
+// 		}
+
+// 		tokenString := cookie // Token is fetched from the cookie
 
 // 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-// 			return cfg.Env.JwtSecretKey.Value(), nil
+// 			return []byte(cfg.Env.JwtSecretKey.Value()), nil
 // 		})
-
 // 		if err != nil {
-// 			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token"})
+// 			util.ClearAuthTokenCookie(c)
+// 			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token", "forceLogout": true})
 // 			c.Abort()
 // 			return
 // 		}
@@ -62,9 +34,36 @@ func VerifyTokenMiddleware(cfg *config.Config) gin.HandlerFunc {
 // 			c.Set("user_id", claims["user_id"])
 // 			c.Next()
 // 		} else {
-// 			c.JSON(401, gin.H{"message": "Unauthorized"})
+// 			util.ClearAuthTokenCookie(c)
+// 			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "forceLogout": true})
 // 			c.Abort()
 // 			return
 // 		}
 // 	}
 // }
+
+func VerifyTokenMiddleware(cfg *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader // Token is directly provided in the Authorization header
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return cfg.Env.JwtSecretKey.Value(), nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("user_id", claims["user_id"])
+			c.Next()
+		} else {
+			c.JSON(401, gin.H{"message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+	}
+}
